e113bf9ffa83abd992e1822b4100f71a869ef9ec
vtuong2212 <thuthe788@gmail.com>
2026-07-30 20:08:53 +0700

    Add ProcessPage class header file

---
+++ RemoteControl_Project/Client/Pages/ProcessPage.h
@@ -0,0 +1,14 @@
+#ifndef PROCESSPAGE_H
+#define PROCESSPAGE_H
+
+#include <QWidget>
+
+class ProcessPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ProcessPage(QWidget *parent = nullptr);
+};
+
+#endif // PROCESSPAGE_H
