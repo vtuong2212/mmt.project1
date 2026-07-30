78faf91a00650395861b16e1f209745865d9d516
vtuong2212 <thuthe788@gmail.com>
2026-07-30 20:08:02 +0700

    Add ApplicationPage header file for QWidget subclass

---
+++ RemoteControl_Project/Client/Pages/ApplicationPage.h
@@ -0,0 +1,14 @@
+#ifndef APPLICATIONPAGE_H
+#define APPLICATIONPAGE_H
+
+#include <QWidget>
+
+class ApplicationPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ApplicationPage(QWidget *parent = nullptr);
+};
+
+#endif // APPLICATIONPAGE_H
