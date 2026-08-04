03e5c368e624d8c32c9e2874a2dd477b39cc8d83
Macbook <huathaiviettuong@MacBook-Air-cua-Macbooktuong2007.local>
2026-08-04 11:49:02 +0700

    Update project

--- RemoteControl_Project/Common/Packet.h
+++ RemoteControl_Project/Common/Packet.h
@@ -4,6 +4,7 @@
 #include <QString>
 #include <QByteArray>
 #include <QDataStream>
+#include <QIODevice>
 
 class Packet
 {
---
+++ RemoteControl_Project/build/Desktop_x86_darwin_generic_mach_o_64bit_Debug/Client/CMakeFiles/RemoteControlClient.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RemoteControlClient_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/RemoteControlClient_autogen.dir/ParseCache.txt"
+  "RemoteControlClient_autogen"
+  "CMakeFiles/RemoteControlClient.dir/GUI/MainWindow.cpp.o"
+  "CMakeFiles/RemoteControlClient.dir/GUI/MainWindow.cpp.o.d"
+  "CMakeFiles/RemoteControlClient.dir/Network/ClientSocket.cpp.o"
+  "CMakeFiles/RemoteControlClient.dir/Network/ClientSocket.cpp.o.d"
+  "CMakeFiles/RemoteControlClient.dir/Pages/ApplicationPage.cpp.o"
+  "CMakeFiles/RemoteControlClient.dir/Pages/ApplicationPage.cpp.o.d"
+  "CMakeFiles/RemoteControlClient.dir/Pages/FilePage.cpp.o"
+  "CMakeFiles/RemoteControlClient.dir/Pages/FilePage.cpp.o.d"
+  "CMakeFiles/RemoteControlClient.dir/Pages/KeyloggerPage.cpp.o"
+  "CMakeFiles/RemoteControlClient.dir/Pages/KeyloggerPage.cpp.o.d"
+  "CMakeFiles/RemoteControlClient.dir/Pages/PowerPage.cpp.o"
+  "CMakeFiles/RemoteControlClient.dir/Pages/PowerPage.cpp.o.d"
+  "CMakeFiles/RemoteControlClient.dir/Pages/ProcessPage.cpp.o"
+  "CMakeFiles/RemoteControlClient.dir/Pages/ProcessPage.cpp.o.d"
+  "CMakeFiles/RemoteControlClient.dir/Pages/ScreenshotPage.cpp.o"
+  "CMakeFiles/RemoteControlClient.dir/Pages/ScreenshotPage.cpp.o.d"
+  "CMakeFiles/RemoteControlClient.dir/Pages/WebcamPage.cpp.o"
+  "CMakeFiles/RemoteControlClient.dir/Pages/WebcamPage.cpp.o.d"
+  "CMakeFiles/RemoteControlClient.dir/RemoteControlClient_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/RemoteControlClient.dir/RemoteControlClient_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/RemoteControlClient.dir/main.cpp.o"
+  "CMakeFiles/RemoteControlClient.dir/main.cpp.o.d"
+  "RemoteControlClient"
+  "RemoteControlClient.pdb"
+  "RemoteControlClient_autogen/mocs_compilation.cpp"
+  "RemoteControlClient_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RemoteControlClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
